3feb1f7dc6a83f282c49eb6db88e8ef4cd4c3071
EmmanuelOchaje <114095241+EmmanuelOchaje@users.noreply.github.com>
2024-11-05 04:19:25 +0100

    commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,11 +8,41 @@ const Contact = (props) => {
             Reach out to me via any of my Socials
           </p>
           <div className="text-white flex justify-center items-center">
-            <i class="bx bxl bxl-instagram"></i>
-            <i class="bx bxl bxl-twitter"></i>
-            <i class="bx bxl bxl-whatsapp"></i>
-            <i class="bx bxl bxl-linkedin"></i>
-            <i class="bx bxl bxl-gmail"></i>
+            <a
+              href="https://instagram.com/dj3rome_"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i class="bx bxl bxl-instagram"></i>
+            </a>
+            <a
+              href="https://twitter.com/adocheee"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i class="bx bxl bxl-twitter"></i>
+            </a>
+            <a
+              href="https://wa.me/2348154498483"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i class="bx bxl bxl-whatsapp"></i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/emmanuel-ochaje-0563482a1/overlay/about-this-profile/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i class="bx bxl bxl-linkedin"></i>
+            </a>
+            <a
+              href="mailto:adocheofficial@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i class="bx bxl bxl-gmail"></i>
+            </a>
             <a
               href="https://github.com/EmmanuelOchaje"
               target="_blank"
